056be2ba536c553133afa22b4639aa1bd6d8f89b
mrtripop <tripop.chai12@gmail.com>
2025-04-19 11:27:27 +0700

    feat: initial spring batch project

---
+++ src/test/java/com/mrtripop/learn/SpringBatchProcessing/SpringBatchProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrtripop.learn.SpringBatchProcessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
